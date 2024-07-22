d58fc7a36b6391d9556da871b8a57de7ccb40c0c
luwai <luwaiwong@gmail.com>
2024-07-22 17:02:44 -0400

    v0.4.1

--- components/pages/Settings.tsx
+++ components/pages/Settings.tsx
@@ -5,7 +5,7 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 import { UserContext } from '../data/UserContext';
 
-const versioncode = "0.4.0";
+const versioncode = "0.4.1";
 
 export default function Settings(){
     let userContext = React.useContext(UserContext);
